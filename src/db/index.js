bd1e81a00c659470328768e0513f75028e6429b9
Fadil Hisyam <ichorvfx@gmail.com>
2025-11-23 14:07:50 +0700

    feat: soft delete setup

--- src/db/index.js
+++ src/db/index.js
@@ -1,3 +1,15 @@
 import { PrismaClient } from '@prisma/client';
+
+const SOFT_DELETE_MODELS = {
+  Difficulty: true,
+  Genre: true,
+  Season: true,
+  Show: true,
+  Region: true,
+  Prefecture: true,
+  City: true,
+  Scene: true,
+};
+
 const prisma = new PrismaClient();
 export default prisma;
