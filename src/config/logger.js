ca0b75ab7aefd686fc659a097412dfb620cf6b92
Fadil Hisyam <ichorvfx@gmail.com>
2026-02-15 17:59:07 +0700

    refactor: modify logger

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,4 +1,17 @@
 import winston from "winston";
+import util from "util";
+
+function safeSerialize(meta) {
+  try {
+    return util.inspect(meta, {
+      depth: 5,
+      breakLength: 120,
+      compact: false,
+    });
+  } catch (err) {
+    return "[Unserializable metadata]";
+  }
+}
 
 export const logger = winston.createLogger({
   level: "info",
@@ -6,8 +19,12 @@ export const logger = winston.createLogger({
     winston.format.timestamp(),
     winston.format.errors({ stack: false }),
     winston.format.printf(({ level, message, timestamp, ...meta }) => {
-      const metaString =
-        Object.keys(meta).length > 0 ? `\n${JSON.stringify(meta, null, 2)}` : '';
+      const hasMeta = meta && Object.keys(meta).length > 0;
+
+      const metaString = hasMeta
+        ? `\n${safeSerialize(meta)}`
+        : "";
+
       return `${timestamp} [${level.toUpperCase()}]: ${message}${metaString}`;
     })
   ),
